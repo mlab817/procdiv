728b2316645b004866f15edeffff90c3e473867b
Mark Lester Bolotaolo <mlab817@gmail.com>
2020-08-11 17:31:14 +0800

    added staff sorting

--- src/store/staff/getters.js
+++ src/store/staff/getters.js
@@ -1,8 +1,9 @@
-export function options(state) {
+export function options(state, getters) {
 	let options = []
+	let sortedStaff = getters.sorted
 
-	Object.keys(state.staff).forEach(key => {
-		const staff = state.staff[key]
+	Object.keys(sortedStaff).forEach(key => {
+		const staff = sortedStaff[key]
 		options.push({
 			label: staff.name,
 			value: staff.name
@@ -10,4 +11,25 @@ export function options(state) {
 	})
 
 	return options
+}
+
+export function sorted(state) {
+	const staffs = state.staff
+	let sorted = {}, keysOrdered = Object.keys(staffs)
+
+	
+	keysOrdered.sort((a, b) => {
+		let aProp = staffs[a].name.toLowerCase(),
+			bProp = staffs[b].name.toLowerCase()
+
+		if (aProp > bProp) return 1
+		else if (aProp < bProp) return -1
+		else return 0
+	})
+
+	keysOrdered.forEach(key => {
+		sorted[key] = staffs[key]
+	})
+
+	return sorted
 }
